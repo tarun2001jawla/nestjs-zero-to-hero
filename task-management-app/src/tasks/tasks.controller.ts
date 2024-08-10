8ac7b9aa3ca1afadd7baf1f4c9bbf4b2b3086c32
tarunjawla <tarunjawla2@gmail.com>
2024-08-10 22:16:52 +0530

    feat: added task model

--- task-management-app/src/tasks/tasks.controller.ts
+++ task-management-app/src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get('/allTasks')
-  getAllTask() {
+  @Get()
+  getAllTask() : Task []  {
     return this.tasksService.getAllTasks();
   }
 }
--- task-management-app/src/tasks/tasks.service.ts
+++ task-management-app/src/tasks/tasks.service.ts
@@ -1,40 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [
+  private tasks: Task[] = [
     {
-      id: 1,
+      id: '1',
       title: 'Buy groceries',
       description: 'Pick up milk, eggs, and bread from the store',
-      completed: false,
+      status: TaskStatus.OPEN,
     },
     {
-      id: 2,
+      id: '2',
       title: 'Finish project report',
       description:
         'Complete the project report by Friday and submit it to the manager',
-      completed: false,
+      status: TaskStatus.IN_PROGRESS,
     },
     {
-      id: 3,
+      id: '3',
       title: 'Book flight tickets',
       description: 'Book flight tickets for the upcoming vacation to Hawaii',
-      completed: false,
+      status: TaskStatus.IN_PROGRESS,
     },
     {
-      id: 4,
+      id: '4',
       title: 'Learn a new language',
       description:
         'Spend 30 minutes each day learning Spanish using a language learning app',
-      completed: false,
+      status: TaskStatus.IN_PROGRESS,
     },
     {
-      id: 5,
+      id: '5',
       title: "Schedule a doctor's appointment",
       description:
         "Schedule a doctor's appointment for a check-up and to discuss any health concerns",
-      completed: false,
+      status: TaskStatus.IN_PROGRESS,
     },
   ];
 
